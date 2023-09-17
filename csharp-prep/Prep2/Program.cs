72972a5faebe17c53b070847dabd1fce18824049
AlfonsoTumbokon <tum21004@byui.edu>
2023-09-16 23:22:16 -0600

    Prep 1 and 2 done!

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade in percentage? ");
+        string ans = Console.ReadLine();
+        int percent = int.Parse(ans);
+
+        string lettergrade = "";
+
+        if (percent >= 90)
+        {
+            lettergrade = "A";
+        }
+        else if (percent >= 80)
+        {
+            lettergrade = "B";
+        }
+        else if (percent >= 70)
+        {
+            lettergrade = "C";
+        }
+        else if (percent >= 60)
+        {
+            lettergrade = "D";
+        }
+        else
+        {
+            lettergrade = "F";
+        }
+
+        Console.WriteLine($"Your grade is {lettergrade}");
+        
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congrats, You Passed!");
+        }
+        else
+        {
+            Console.WriteLine("You Failed!");
+        }
     }
 }
