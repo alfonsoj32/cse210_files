1600358e86b31d13c89dd8e3f6010142e58f2fde
AlfonsoTumbokon <tum21004@byui.edu>
2023-09-20 17:02:52 -0600

    W02 Prove: Developer—C# Programming Parts 3–5

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int random_num = randomGenerator.Next(1, 101);
+        int guess = -1;
+
+        while (guess != random_num)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (random_num > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else if (random_num < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,58 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<double> numbers = new List<double>();
+        
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (true)
+        {
+            Console.Write("Enter number: ");
+            double input;
+            
+            if (double.TryParse(Console.ReadLine(), out input))
+            {
+                if (input == 0)
+                    break;
+
+                numbers.Add(input);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+        }
+
+        if (numbers.Count == 0)
+        {
+            Console.WriteLine("No numbers entered.");
+        }
+        else
+        {
+            double total = 0;
+            foreach (double num in numbers)
+            {
+                total += num;
+            }
+
+            double average = total / numbers.Count;
+            double maxNumber = numbers[0];
+
+            foreach (double num in numbers)
+            {
+                if (num > maxNumber)
+                {
+                    maxNumber = num;
+                }
+            }
+
+            Console.WriteLine($"The sum is: {total}");
+            Console.WriteLine($"The average is: {average}");
+            Console.WriteLine($"The largest number is: {maxNumber}");
+        }
     }
 }
